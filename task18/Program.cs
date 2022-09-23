f46473db00557ee76fbb90b1bb4f9795cad69614
Adelaida <adanebydet@gmail.com>
2022-09-23 11:12:31 +0300

    n

---
+++ task18/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напиште программу, которая по заданному номеру четверти, показывает диапозон возможных координат точек в этой четверти (X и Y).
+
+Console.WriteLine("Введите номер четверти");
+string x = Console.ReadLine();
+
+string Quater(string xc)
+{
+    if (xc == "1") return "x>0, y>0";
+    if (xc == "2") return "x<0, y>0";
+    if (xc == "3") return "x<0, y<0";
+    if (xc == "4") return "x>0, y<0";
+    return "Введите число от 1 до 4";
+}
+String result = Quater(x);
+Console.WriteLine(result);
